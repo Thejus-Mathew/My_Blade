b933cd3c268facbc346549fa78ed69ad8c616d08
I-DUDE-I <thejusmathew8888@gmail.com>
2025-06-03 23:24:08 +0530

    fixed settle dues filteer

--- src/app/insights/page.jsx
+++ src/app/insights/page.jsx
@@ -7,19 +7,13 @@ export const dynamic = "force-dynamic";
 
 export default async function InsightsPage({searchParams}) {
   const params = await searchParams
-  
-  if(!params?.startDate){
-    const now = new Date();
-    const firstDay = `${now.getFullYear()}-${String(now.getMonth() + 1).padStart(2, '0')}-01`;    
-    params.startDate = firstDay
-  }
   const [expensesResponse,membersResponse,typesResponse] = await Promise.all([getAllExpensesAction(params),getMembersAction(),getTypesAction()])
   
   return (
     <div className="max-w-6xl mx-auto ">
         <h1 className="text-3xl font-bold mb-6">Insights</h1>
-        <InsightFilter members={membersResponse.data || []} types={(typesResponse.data || []).filter(item=>item.name!=='Settle Due')} params = {params} />
-        <InsightDisplay members={membersResponse.data || []} expenses={(expensesResponse || []).filter(item=>item.type!=='Settle Due')}/>
+        <InsightFilter members={membersResponse.data || []} types={(typesResponse.data || []).filter(item=>item.name!=='Settle Dues')} params = {params} />
+        <InsightDisplay members={membersResponse.data || []} expenses={(expensesResponse || []).filter(item=>item.type!=='Settle Dues')}/>
     </div>
   )
 }
